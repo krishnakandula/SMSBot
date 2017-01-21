572c3f9544037bd652bc6c16ce91e5c97ab12be3
krishnakandula <krishna.c.kandula@gmail.com>
2017-01-20 20:22:04 -0600

    Basic functionality working

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,8 +1,20 @@
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 /**
  * Created by krishnakandula on 1/20/17.
  */
 public class Driver {
-    public static void main(String... args){
+    private static final String TO_PHONE_NUMBER = "2146979165";
 
+    public static void main(String... args){
+        Twilio.init(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_AUTH_TOKEN);
+
+        PhoneNumber fromPhoneNumber = new PhoneNumber(Config.TWILIO_FROM_PHONE_NUMBER);
+        PhoneNumber toPhoneNumber = new PhoneNumber(TO_PHONE_NUMBER);
+        Message message = Message.creator(toPhoneNumber, fromPhoneNumber, "Hi Sexy")
+                .create();
+        System.out.println(message.getSid());
     }
 }
